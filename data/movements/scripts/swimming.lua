7c3a8cf5112cfe1640c71cf48ee4a6956f62b73d
Yohan Augusto <yohan_augusto@hotmail.com>
2020-04-30 15:21:22 -0300

    update base mov

--- data/movements/scripts/swimming.lua
+++ data/movements/scripts/swimming.lua
@@ -1,35 +1,21 @@
-local outfit = {lookType = 267, lookHead = 0, lookBody = 0, lookLegs = 0, lookFeet = 0, lookTypeEx = 0, lookAddons = 0}
+local condition = Condition(CONDITION_OUTFIT)
+condition:setOutfit({lookType = 267})
+condition:setTicks(-1)
 
-function onStepIn(cid, item, position, lastPosition, fromPosition, toPosition, actor)
-	if(hasCondition(cid, CONDITION_OUTFIT, 0, CONDITIONID_COMBAT) and getCreatureOutfit(cid).lookType == outfit.lookType) then
-		doRemoveCondition(cid, CONDITION_OUTFIT)
-		if(not isPlayerGhost(cid)) then
-			doSendMagicEffect(position, CONST_ME_POFF)
-		end
+function onStepIn(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return false
 	end
 
+	creature:addCondition(condition)
 	return true
 end
 
-function onStepOut(cid, item, position, lastPosition, fromPosition, toPosition, actor)
-	if(not isPlayer(cid)) then
-		return true
+function onStepOut(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return false
 	end
 
-	local tmp = getTileInfo(toPosition)
-	if(tmp.trashHolder) then
-		if(doTileQueryAdd(cid, toPosition, 4) ~= RETURNVALUE_NOERROR) then
-			return false
-		end
-
-		if(not isPlayerGhost(cid)) then
-			doSendMagicEffect(fromPosition, CONST_ME_POFF)
-			doSendMagicEffect(toPosition, CONST_ME_WATERSPLASH)
-		end
-
-		doRemoveConditions(cid, true)
-		doSetCreatureOutfit(cid, outfit, -1)
-	end
-
+	creature:removeCondition(CONDITION_OUTFIT)
 	return true
 end
