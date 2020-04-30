e0adf8ee78a8f9acbf3bc4b2e3196c491efa9f45
Yohan Augusto <yohan_augusto@hotmail.com>
2020-04-30 16:58:39 -0300

    Revert "update npc"
    This reverts commit 5cda82a3af83ac51dc5c4e40f86fce4d34904b8b.

--- data/npc/lib/npc.lua
+++ data/npc/lib/npc.lua
@@ -1,195 +1,120 @@
-function selfSayChannel(cid, message)
-	return selfSay(message, cid, false)
+-- Including the Advanced NPC System
+dofile('data/npc/lib/npcsystem/npcsystem.lua')
+dofile('data/npc/lib/npcsystem/customModules.lua')
+
+isPlayerPremiumCallback = Player.isPremium
+
+function msgcontains(message, keyword)
+	local message, keyword = message:lower(), keyword:lower()
+	if message == keyword then
+		return true
+	end
+
+	return message:find(keyword) and not message:find('(%w+)' .. keyword)
 end
 
-function selfMoveToThing(id)
-	errors(false)
-	local thing = getThing(id)
+function doNpcSellItem(cid, itemId, amount, subType, ignoreCap, inBackpacks, backpack)
+	local amount = amount or 1
+	local subType = subType or 0
+	local item = 0
+	local player = Player(cid)
 
-	errors(true)
-	if(thing.uid == 0) then
-		return
-	end
-
-	local t = getThingPosition(id)
-	selfMoveTo(t.x, t.y, t.z)
-	return
-end
-
-function selfMoveTo(x, y, z)
-	local position = {x = 0, y = 0, z = 0}
-	if(type(x) ~= "table") then
-		position = Position(x, y, z)
-	else
-		position = x
-	end
-
-	if(isValidPosition(position)) then
-		doSteerCreature(getNpcId(), position)
-	end
-end
-
-function selfMove(direction, flags)
-	local flags = flags or 0
-	doMoveCreature(getNpcId(), direction, flags)
-end
-
-function selfTurn(direction)
-	doCreatureSetLookDirection(getNpcId(), direction)
-end
-
-function getNpcDistanceTo(id)
-	errors(false)
-	local thing = getThing(id)
-
-	errors(true)
-	if(thing.uid == 0) then
-		return nil
-	end
-
-	local c = getCreaturePosition(id)
-	if(not isValidPosition(c)) then
-		return nil
-	end
-
-	local s = getCreaturePosition(getNpcId())
-	if(not isValidPosition(s) or s.z ~= c.z) then
-		return nil
-	end
-
-	return math.max(math.abs(s.x - c.x), math.abs(s.y - c.y))
-end
-
-function doMessageCheck(message, keyword, exact)
-	local exact = exact or false
-	if(type(keyword) == "table") then
-		return isInArray(keyword, message, exact)
-	end
-
-	if(exact) then
-		return message == keyword
-	end
-
-	local a, b = message:lower(), keyword:lower()
-	return a == b or (a:find(b) and not a:find('(%w+)' .. b))
-end
-
-function doNpcSellItem(cid, itemid, amount, subType, ignoreCap, inBackpacks, backpack)
-	local amount, subType, ignoreCap, inBackpacks, backpack  = amount or 1, subType or 0, ignoreCap or false, inBackpacks or false, backpack or 1988
-
-	local exhaustionNPC = getBooleanFromString(getConfigValue('exhaustionNPC'))
-	if(exhaustionNPC) then
-		local exhaustionInSeconds = getConfigValue('exhaustionInSecondsNPC')
-		local storage = 45814
-		if(exhaustion.check(cid, storage) == true) then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You cant buy it so fast.")
-			return false
-		end
-		exhaustion.set(cid, storage, exhaustionInSeconds)
-	end
-
-	local item, a = nil, 0
-	if(inBackpacks) then
-		local custom, stackable = 1, isItemStackable(itemid)
-		if(stackable) then
-			custom = math.max(1, subType)
-			subType = amount
-			amount = math.max(1, math.floor(amount / 100))
+	if ItemType(itemId):isStackable() then
+		local stuff
+		if inBackpacks then
+			stuff = Game.createItem(backpack, 1)
+			item = stuff:addItem(itemId, math.min(100, amount))
+		else
+			stuff = Game.createItem(itemId, math.min(100, amount))
 		end
 
-		local container, b = doCreateItemEx(backpack, 1), 1
-		for i = 1, amount * custom do
-			item = doAddContainerItem(container, itemid, subType)
-			if(itemid == ITEM_PARCEL) then
-				doAddContainerItem(item, ITEM_LABEL)
-			end
+		return player:addItemEx(stuff, ignoreCap) ~= RETURNVALUE_NOERROR and 0 or amount, 0
+	end
 
-			if(isInArray({(getContainerCapById(backpack) * b), amount}, i)) then
-				if(doPlayerAddItemEx(cid, container, ignoreCap) ~= RETURNVALUE_NOERROR) then
+	local a = 0
+	if inBackpacks then
+		local container, b = Game.createItem(backpack, 1), 1
+		for i = 1, amount do
+			local item = container:addItem(itemId, subType)
+			if table.contains({(ItemType(backpack):getCapacity() * b), amount}, i) then
+				if player:addItemEx(container, ignoreCap) ~= RETURNVALUE_NOERROR then
 					b = b - 1
 					break
 				end
 
 				a = i
-				if(amount > i) then
-					container = doCreateItemEx(backpack, 1)
+				if amount > i then
+					container = Game.createItem(backpack, 1)
 					b = b + 1
 				end
 			end
 		end
 
-		if(not stackable) then
-			return a, b
-		end
-
-		return (a * subType / custom), b
+		return a, b
 	end
 
-	if(isItemStackable(itemid)) then
-		a = amount * math.max(1, subType)
-		repeat
-			local tmp = math.min(100, a)
-			item = doCreateItemEx(itemid, tmp)
-			if(doPlayerAddItemEx(cid, item, ignoreCap) ~= RETURNVALUE_NOERROR) then
-				return 0, 0
-			end
-
-			a = a - tmp
-		until a == 0
-		return amount, 0
-	end
-
-	for i = 1, amount do
-		item = doCreateItemEx(itemid, subType)
-		if(itemid == ITEM_PARCEL) then
-			doAddContainerItem(item, ITEM_LABEL)
-		end
-
-		if(doPlayerAddItemEx(cid, item, ignoreCap) ~= RETURNVALUE_NOERROR) then
+	for i = 1, amount do -- normal method for non-stackable items
+		local item = Game.createItem(itemId, subType)
+		if player:addItemEx(item, ignoreCap) ~= RETURNVALUE_NOERROR then
 			break
 		end
-
 		a = i
 	end
-
 	return a, 0
 end
 
-function doRemoveItemIdFromPosition(id, n, position)
-	local thing = getTileItemById(position, id)
-	if(thing.itemid < 101) then
+local func = function(cid, text, type, e, pcid)
+	local npc = Npc(cid)
+	if not npc then
+		return
+	end
+
+	local player = Player(pcid)
+	if player:isPlayer() then
+		local creature = Creature(cid)
+		creature:say(text, type, false, pcid, creature:getPosition())
+		e.done = true
+	end
+end
+
+function doCreatureSayWithDelay(cid, text, type, delay, e, pcid)
+	if Player(pcid):isPlayer() then
+		e.done = false
+		e.event = addEvent(func, delay < 1 and 1000 or delay, cid, text, type, e, pcid)
+	end
+end
+
+function doPlayerSellItem(cid, itemid, count, cost)
+	local player = Player(cid)
+	if player:removeItem(itemid, count) then
+		if not player:addMoney(cost) then
+			error('Could not add money to ' .. player:getName() .. '(' .. cost .. 'gp)')
+		end
+		return true
+	end
+	return false
+end
+
+function doPlayerBuyItemContainer(cid, containerid, itemid, count, cost, charges)
+	local player = Player(cid)
+	if not player:removeMoney(cost) then
 		return false
 	end
 
-	doRemoveItem(thing.uid, n)
+	for i = 1, count do
+		local container = Game.createItem(containerid, 1)
+		for x = 1, ItemType(containerid):getCapacity() do
+			container:addItem(itemid, charges)
+		end
+
+		if player:addItemEx(container, true) ~= RETURNVALUE_NOERROR then
+			return false
+		end
+	end
 	return true
 end
 
-function getNpcName()
-	return getCreatureName(getNpcId())
+function getCount(string)
+	local b, e = string:find("%d+")
+	return b and e and tonumber(string:sub(b, e)) or -1
 end
-
-function getNpcPosition()
-	return getThingPosition(getNpcId())
-end
-
-function selfGetPosition()
-	local t = getThingPosition(getNpcId())
-	return t.x, t.y, t.z
-end
-
-msgcontains = doMessageCheck
-moveToPosition = selfMoveTo
-moveToCreature = selfMoveToThing
-selfMoveToCreature = selfMoveToThing
-selfMoveToPosition = selfMoveTo
-isPlayerPremiumCallback = isPremium
-doPosRemoveItem = doRemoveItemIdFromPosition
-doRemoveItemIdFromPos = doRemoveItemIdFromPosition
-doNpcBuyItem = doPlayerRemoveItem
-doNpcSetCreatureFocus = selfFocus
-getNpcCid = getNpcId
-getDistanceTo = getNpcDistanceTo
-getDistanceToCreature = getNpcDistanceTo
-getNpcDistanceToCreature = getNpcDistanceTo
-getNpcPos = getNpcPosition
